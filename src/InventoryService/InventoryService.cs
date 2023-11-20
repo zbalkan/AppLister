0264acc36fc9e5614e48fa9eec4c2cee675e7752
Zafer Balkan <zafer@zaferbalkan.com>
2023-11-20 23:04:58 +0200

    Added another log for performance reasons.

--- src/InventoryService/InventoryService.cs
+++ src/InventoryService/InventoryService.cs
@@ -5,30 +5,35 @@ namespace InventoryService
     public sealed class InventoryService
     {
         private readonly EventLog _logger;
-        private readonly WmiScanner wmiScanner;
         private readonly Discovery discovery;
-
+        private readonly Stopwatch stopwatch;
+        private readonly WmiScanner wmiScanner;
         public InventoryService(EventLog logger)
         {
             _logger = logger;
             wmiScanner = new WmiScanner();
             discovery = new Discovery();
+            stopwatch = new Stopwatch();
         }
 
         public void Refresh()
         {
-            _logger.WriteEntry("Reading packages from WMI.", EventLogEntryType.Information);
+            _logger?.WriteEntry("Reading packages from WMI.", EventLogEntryType.Information);
             var publishedPackages = wmiScanner.GetAll();
 
-            _logger.WriteEntry("Running discovery scan.", EventLogEntryType.Information);
+            _logger?.WriteEntry("Running discovery scan.", EventLogEntryType.Information);
+            stopwatch.Reset();
+            stopwatch.Start();
             var discoveredPackages = discovery.GetAll();
+            stopwatch.Stop();
+            _logger?.WriteEntry($"Discovery scan completed. Elapsed time: {stopwatch.Elapsed}.", EventLogEntryType.Information);
 
             if (publishedPackages?.Count > 0)
             {
-                _logger.WriteEntry("Cleaning inventory.", EventLogEntryType.Information);
+                _logger?.WriteEntry("Cleaning inventory.", EventLogEntryType.Information);
                 Publisher.Unpublish(publishedPackages);
             }
-            _logger.WriteEntry("Updating inventory.", EventLogEntryType.Information);
+            _logger?.WriteEntry("Updating inventory.", EventLogEntryType.Information);
             Publisher.Publish(discoveredPackages);
         }
     }
