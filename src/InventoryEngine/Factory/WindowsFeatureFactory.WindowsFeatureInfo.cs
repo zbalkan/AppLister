5edf89e763e6b35540c7fc8dce899668d6ad316f
Zafer Balkan <zafer@zaferbalkan.com>
2025-02-21 11:41:18 +0200

    Removed unused proeprties

--- src/InventoryEngine/Factory/WindowsFeatureFactory.WindowsFeatureInfo.cs
+++ src/InventoryEngine/Factory/WindowsFeatureFactory.WindowsFeatureInfo.cs
@@ -6,17 +6,10 @@ namespace InventoryEngine.Factory
     {
         private class WindowsFeatureInfo
         {
-            public WindowsFeatureInfo()
-            {
-                CustomProperties = new List<KeyValuePair<string, string>>();
-            }
-
             public string FeatureName { get; set; }
             public string DisplayName { get; set; }
             public string Description { get; set; }
-            public string RestartRequired { get; set; }
             public bool Enabled { get; set; }
-            public IList<KeyValuePair<string, string>> CustomProperties { get; private set; }
         }
     }
 }
